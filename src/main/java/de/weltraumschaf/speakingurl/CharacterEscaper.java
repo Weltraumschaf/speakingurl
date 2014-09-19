ae9f6eceb6cad15e934f634b84580310db63f6ff
Sven Strittmatter <ich@weltraumschaf.de>
2014-09-19 06:25:48 +0200

    Add character escaper.

---
+++ src/main/java/de/weltraumschaf/speakingurl/CharacterEscaper.java
@@ -0,0 +1,25 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ * 
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+package de.weltraumschaf.speakingurl;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+final class CharacterEscaper {
+    private static final String regex = "[-\\\\^$*+?.()|\\[\\]{}\\/]";
+    
+    String escape(final String input) {
+        return input.replaceAll(regex, "\\\\$&");
+    }
+    
+}
