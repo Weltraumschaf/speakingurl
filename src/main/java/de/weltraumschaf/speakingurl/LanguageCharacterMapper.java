ee8a8e615357c57de972bbc5a86e72babc0e4aea
Sven Strittmatter <ich@weltraumschaf.de>
2014-09-28 22:18:55 +0200

    Add missing doc and validation.

--- src/main/java/de/weltraumschaf/speakingurl/LanguageCharacterMapper.java
+++ src/main/java/de/weltraumschaf/speakingurl/LanguageCharacterMapper.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Language specific characters translations
+ * Language specific characters translations.
  *
  * @author Sascha Droste <pid@posteo.net>
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
@@ -31,7 +31,21 @@ final class LanguageCharacterMapper {
         MAPPING.put(Language.SWEDISH, Collections.unmodifiableMap(swedish));
     }
 
+    /**
+     * Input validator.
+     */
+    private final Validator validator = new Validator();
+
+    /**
+     * Tells if there is a mapping for a particular language.
+     *
+     * @param lang must not be {@code null}
+     * @param ch you want to map, must not be {@code null} or empty
+     * @return {@code true} if the given character has a mapping, else {@code false}
+     */
     boolean knowsCharacter(final Language lang, final String ch) {
+        validator.notNull(lang, "validator");
+
         if (MAPPING.containsKey(lang)) {
             return MAPPING.get(lang).containsKey(ch);
         }
@@ -39,6 +53,17 @@ final class LanguageCharacterMapper {
         return MAPPING.get(DEFAULT).containsKey(ch);
     }
 
+    /**
+     * Maps given character for a particular language.
+     * <p>
+     * Throws an {@link IllegalArgumentException} if {@link #knowsCharacter(de.weltraumschaf.speakingurl.Language,
+     * java.lang.String)  there is no mapping}.
+     * </p>
+     *
+     * @param lang must not be {@code null}
+     * @param ch you want to map, must not be {@code null} or empty
+     * @return never {@code null}
+     */
     String mapCharacter(final Language lang, final String ch) {
         if (knowsCharacter(lang, ch)) {
             return MAPPING.get(lang).get(ch);
