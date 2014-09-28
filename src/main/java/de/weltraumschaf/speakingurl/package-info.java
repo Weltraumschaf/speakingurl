4d1ff72199580477f5887f7bc46a2f8575f2b673
Sven Strittmatter <ich@weltraumschaf.de>
2014-09-28 22:21:29 +0200

    Add package info.

---
+++ src/main/java/de/weltraumschaf/speakingurl/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Main package.
+ * <p>
+ * Public API is:
+ * </p>
+ * <ul>
+ *  <li>{@link Slug}</li>
+ *  <li>{@link Slug.Builder}</li>
+ *  <li>{@link Language}</li>
+ * </ul>
+ */
+package de.weltraumschaf.speakingurl;
